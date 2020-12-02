b12de09fd4cd5c9cd36e3b3a49368e293d485e99
Clemens Pircher <clemens.lukas@gmx.at>
2020-12-02 18:35:01 +0100

    adapted dummy cop

--- hardware/src/main/scala/cop/dummy.scala
+++ hardware/src/main/scala/cop/dummy.scala
@@ -17,36 +17,48 @@ object Dummy extends CoprocessorObject {
 class Dummy() extends CoprocessorMemory() {
   //coprocessor definitions
   def FUNC_ADD = "b00000".U(5.W)
-  def FUNC_VECTOR_ADD = "b00002".U(5.W)
+  def FUNC_ADD_STALL = "b00001".U(5.W)
+  //def FUNC_VECTOR_ADD = "b00002".U(5.W)
 
   //some coprocessor registers
-  val resultReg = Reg(init = UInt(0, 32))
-  val busy = Reg(init = Bool(false))
+  val idle :: scalar_add :: Nil = Enum(2)
+  val state = Reg(init = idle)
 
+  io.copOut.result := UInt(0)
   io.copOut.ena_out := Bool(false)
 
-
-  when(io.copIn.trigger) {
+  // start operation
+  when(io.copIn.trigger & io.copIn.ena_in) {
     // read or custom command
-    when(io.copIn.read){
-      when(busy){
-        io.copOut.ena_out := Bool(true)
+    when(io.copIn.read) {
+      when(state === idle) {
+        // 0-latency add
+        when(io.copIn.funcId === FUNC_ADD) {
+          io.copOut.result := io.copIn.opData(0) + io.copIn.opData(1)
+          io.copOut.ena_out := Bool(true)
+        }
+		// 1-latency add
+        when(io.copIn.funcId === FUNC_ADD_STALL) {
+          state := scalar_add
+        }
       }
-      when(io.copIn.funcId === FUNC_ADD) {
-        resultReg := io.copIn.opData(0) + io.copIn.opData(1)
-      }
-      io.copOut.result := resultReg
-    //write command
-    }.otherwise{
+    }
+  	//write command: TODO
+	/*.otherwise{
       when(io.copIn.funcId === FUNC_VECTOR_ADD) {
         busy := Bool(true)
         //read from memory
         //memPort.MCmd := memPort.RD
         busy := Bool(false)
       }
-    }
+    }*/
   }
-  when(io.copIn.ena_in) {
-
+  
+  // output logic for 1-latency add
+  when(io.copIn.ena_in & state === scalar_add) {
+    io.copOut.result := io.copIn.opData(0) + io.copIn.opData(1)
+    io.copOut.ena_out := Bool(true)
+    state := idle
   }
+  
 }
