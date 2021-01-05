1e976148ea40d7c05b39732f500c2bd245cc2b38
ECS EDA User <edauser@localhost.localdomain>
2021-01-05 11:47:47 +0100

    changes test add file to include delay operations

--- c/test_cop_add.c
+++ c/test_cop_add.c
@@ -2,22 +2,45 @@
 
 int main() {
 
-	int opa = 240;
-	int opb = 255;
+	int opa = 1;
+	int opb = 2;
+	int opc = 4;
+	int opd = 8;
 	int res = 0;
 
-	asm (	".word   52\n\t"
-		"mov $r4 = %[a]\n\t"
-		"mov $r3 = %[b]\n\t"
-		".word 0x034A4180\n\t"
+	
+	// adds all operands together
+	// add r1 r2 -> r3
+	// add r3 r4 -> r5
+	// add r5 r6 -> r7
+	asm (	".word   88\n\t"
+		"mov $r1 = %[a]\n\t"
+		"mov $r2 = %[b]\n\t"
+		"mov $r4 = %[c]\n\t"
+		"mov $r6 = %[d]\n\t"
+		//add without delay
+		".word 0x03461100\n\t"
+		".word 0x034A3200\n\t"
+		".word 0x034E5300\n\t"
 		"nop\n\t"
-		"mov %[d] = $r5\n\t"
+		"nop\n\t"
+		"nop\n\t"
+		//reset registers
+		"mov $r1 = %[a]\n\t"
+		"mov $r2 = %[b]\n\t"
+		"mov $r4 = %[c]\n\t"
+		"mov $r6 = %[d]\n\t"
+		//add with delay
+		".word 0x03461102\n\t"
+		".word 0x034A3202\n\t"
+		".word 0x034E5302\n\t"
+		"mov %[res] = $r7\n\t"
 		"nop\n\t"
 		"nop\n\t"
 		"nop\n\t"
-		: [d] "=r" (res)
-		: [a] "r" (opa), [b] "r" (opb)
-		: "3", "4", "5" );
+		: [res] "=r" (res)
+		: [a] "r" (opa), [b] "r" (opb), [c] "r" (opc), [d] "r" (opd)
+		: "1", "2", "3", "4", "5", "6", "7" );
 }
 
 
