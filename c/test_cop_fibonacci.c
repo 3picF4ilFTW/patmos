fcebbb21583cf07610b5b3a79e4033ea18b744c9
Christoph Lehr <christoph.lehr@gmx.at>
2021-01-13 16:05:09 +0100

    added print statements to examples

--- c/test_cop_fibonacci.c
+++ c/test_cop_fibonacci.c
@@ -1,33 +1,35 @@
-//#include "include/bootable.h"
+#include <stdio.h>
 
 int main() {
 
-	int opa = 5;
 	int res = 0;
+	for(int i=1; i<=45; i++)
+	{
+		asm (
+			"mov $r3 = %[a]\n\t"
+			".word 0x03443011\n\t"
+			"nop\n\t"
+			"nop\n\t"
+			"nop\n\t"
+			"nop\n\t"
+			"nop\n\t"
+			"nop\n\t"
 
-	asm (	".word   52\n\t"
-		"mov $r3 = %[a]\n\t"
-		".word 0x03443011\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
-		"nop\n\t"
+			".word 0x03480113\n\t"
+			"mov %[d] = $r4\n\t"
 
-		".word 0x03480113\n\t"
-		"mov %[d] = $r4\n\t"
+			"nop\n\t"
 
-		"nop\n\t"
+			".word 0x03443011\n\t"
+			"nop\n\t"
+			".word 0x03480113\n\t"
+			"mov %[d] = $r4\n\t"
 
-		".word 0x03443011\n\t"
-		"nop\n\t"
-		".word 0x03480113\n\t"
-		"mov %[d] = $r4\n\t"
-
-		: [d] "=r" (res)
-		: [a] "r" (opa)
-		: "3", "4", "5" );
+			: [d] "=r" (res)
+			: [a] "r" (i)
+			: "3", "4", "5" );
+		printf("Fibonacci sequence after %d iterations: %d\n", i, res);
+	}
 }
 
 
