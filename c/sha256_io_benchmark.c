2b317f650369d69d999a18bdeb216ddbfb872713
ECS EDA User <edauser@localhost.localdomain>
2021-04-15 14:33:38 +0200

    added timing to benchmark

--- c/sha256_io_benchmark.c
+++ c/sha256_io_benchmark.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
+/*----------------------------------------DEFINITIONS FOR SHA-256 IO-DEVICE------------------------------------------*/
 #define HASH_WORDS (8)                                        // 8 words per hash
 #define BLOCK_WORDS (16)                                      // 16 words per data block
 #define TERMINATION_WORDS (2)                                 // 2 words required for length termination
@@ -28,14 +29,14 @@ uint32_t msg_blocks = 0;
 uint32_t msg_block_cursor = 0;
 
 // Copy message into message state and pad
-// Returns true on success
-bool pad_message(char *str, int32_t len)
+// Returns number of blocks on success and 0 on failure
+uint32_t pad_message(const char *str, int32_t len)
 {
   if (len < 0)
     len = strlen(str);
     
   if (len > MAX_MSG_BYTES - PADDING_BYTES - TERMINATION_BYTES)
-    return false;
+    return 0;
 
   // copy the message and append padding byte
   memcpy(msg_buf, str, len);
@@ -55,14 +56,13 @@ bool pad_message(char *str, int32_t len)
     // we can simply terminate the last block
     memset(msg_buf + len + 1, 0x00, last_block_padding - PADDING_BYTES - TERMINATION_BYTES);
   }
-  // all length-termination words except for the last one are 0 because we only allow int for the length
   memset(msg_buf + newlen - TERMINATION_BYTES, 0x00, TERMINATION_BYTES - sizeof(int64_t));
-  int64_t bit_len = len << 3;
+  int64_t bit_len = ((int64_t)len) << 3; // multiply by 8 because we usually state the length in bits 
   memcpy(msg_buf + newlen - sizeof(int64_t), &bit_len, sizeof(int64_t));
   msg_blocks = newlen / BLOCK_BYTES;
   
   // new state is now stored in msg_buf and msg_blocks
-  return true;
+  return msg_blocks;
 }
 
 // reset SHA-256 seed and transmission state
@@ -114,32 +114,121 @@ void busy_wait()
   while(*sha_ptr);
 }
 
-/*-------------------------------------------------------------------------------------------------------------------*/
-int main(int argc, char **argv) {
-  // set message
-  pad_message("abc", -1);
-  
+/*-----------------------------------------DEFINITIONS FOR TIMER IO-DEVICE-------------------------------------------*/
+// define timer IO-device pointer
+_iodev_ptr_t timer_ptr_high = (_iodev_ptr_t)(PATMOS_IO_TIMER);
+_iodev_ptr_t timer_ptr_low = (_iodev_ptr_t)(PATMOS_IO_TIMER + 0x04);
+
+uint32_t get_time32()
+{
+  return *timer_ptr_low;
+}
+
+// TODO: maybe replace this with some inline asm code
+uint64_t get_time64()
+{
+  uint64_t ret = *timer_ptr_low;
+  ret |= ((uint64_t)*timer_ptr_high) << 32;
+  return ret;
+}
+
+/*-------------------------------------------DEFINITIONS FOR BENCHMARKING--------------------------------------------*/
+void benchmark_hash(uint32_t *hash, uint32_t *busy_time_s, uint32_t *busy_time_r, uint32_t *idle_time)
+{  
   // perform SHA-256 device reset
   reset();
   
+  uint32_t busy_acc_s = 0;
+  uint32_t busy_acc_r = 0;
+  uint32_t idle_acc = 0;
+  uint32_t busy_start;
+  uint32_t idle_start;
+  
+  busy_start = get_time32();
   // send blocks
   while(!send_next_block())
   {
+    idle_start = get_time32();
+    busy_acc_s += (idle_start - busy_start);
+    
     busy_wait();
+    
+    busy_start = get_time32();
+    idle_acc += (busy_start - idle_start);
+    busy_start = get_time32();  // correction to account for previous line
   }
+  idle_start = get_time32();
+  busy_acc_s += (idle_start - busy_start);
+  
+  busy_wait();
+  
+  busy_start = get_time32();
+  idle_acc += (busy_start - idle_start);
+  busy_start = get_time32();  // correction to account for previous line
   
   // hash array
-  uint32_t hash[HASH_WORDS];
-  busy_wait();
   retrieve_hash(hash);
   
-  // print the hash
+  idle_start = get_time32();
+  busy_acc_r += (idle_start - busy_start);
+  
+  *busy_time_s = busy_acc_s;
+  *busy_time_r = busy_acc_r;
+  *idle_time = idle_acc;
+}
+
+void print_hash(uint32_t *hash)
+{
   char *hash_bytes = (char *)hash;
   for(int32_t i = 0; i < HASH_BYTES; ++i)
   {
     printf("%.2hhx ", hash_bytes[i]);
   }
   fflush(stdout);
+}
+
+/*---------------------------------------------------MAIN PROGRAM----------------------------------------------------*/
+#define REPETITIONS (3)
+
+const char *benchmark_strings[] = { "",
+                                    "abc",
+                                    "This is a message which barely fits into one data block",
+                                    "This is a message which already requires two data blocks",
+                                    "We need three data blocks as soon as the message is at least 120 characters long"
+                                    " but also not longer than 183 characters"};
+
+uint32_t benchmark_string_count = sizeof(benchmark_strings) / sizeof(benchmark_strings[0]);
+
+int main(int argc, char **argv) {
+  uint32_t hash[HASH_WORDS];
+  uint32_t busy_time_s;
+  uint32_t busy_time_r;
+  uint32_t idle_time;
+  bool success;
+  
+  for(uint32_t i = 0; i < benchmark_string_count; ++i)
+  {
+    const char *str = benchmark_strings[i];
+    uint32_t block_count = pad_message(str, -1);
+    if(block_count == 0)
+    {
+      printf("could not process string \"%s\"\n\n", str);
+      continue;
+    }
+    
+    printf("benchmarking %ld blocks for string \"%s\"\n", block_count, str);
+    for(uint32_t j = 0; j < REPETITIONS; ++j)
+    {
+      printf("  repetition %ld\n", j);
+      benchmark_hash(hash, &busy_time_s, &busy_time_r, &idle_time);
+      printf("    busy_send_time: %lu\n"
+             "    busy_recv_time: %lu\n"
+             "    idle_time:      %lu\n", busy_time_s, busy_time_r, idle_time);
+    }
+    fputs("\thash: ", stdout);
+    print_hash(hash);
+    puts("\n");
+  }
   
   return 0;
 }
